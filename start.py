c3538d700519384b21aab9f324aaa0812c98121b
AYUSH <107471238+JustAyu@users.noreply.github.com>
2024-02-07 10:36:19 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,23 @@
+import asyncio
+from pyrogram import filters, Client
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message
+
+API_ID = 
+ALI_HASH = ""
+app = Client("babe",api_id=API_ID,api_hash=API_HASH)
+
+ASKING = int(input("How many dps you wanna delete?\n"))
+
+
+async def sux(client, message):
+  try:
+    photos = [p async for p in app.get_chat_photos("me")]
+    print("Deleting All Photos...")
+    await app.delete_profile_photos([p.file_id for p in photos[1:ASKING]])
+    print("Deleted All Photos!")
+  except:
+    print("Something Is Wrong\nERROR!")
+
+
+app.run(sux())
